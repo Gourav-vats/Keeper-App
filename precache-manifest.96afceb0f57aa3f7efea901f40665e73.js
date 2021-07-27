e5ccffaa74d0772bd5830041d1cab9a4caee60aa
Gourav Vats <gourav4142@gmail.com>
2021-07-27 22:07:25 +0530

    Updates

---
+++ precache-manifest.96afceb0f57aa3f7efea901f40665e73.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "471f3129393a5c46341a444b36017ba0",
+    "url": "/Keeper-App/index.html"
+  },
+  {
+    "revision": "731e4757498e8894696a",
+    "url": "/Keeper-App/static/js/2.5dc2e22d.chunk.js"
+  },
+  {
+    "revision": "12933e444d1599c63f7e",
+    "url": "/Keeper-App/static/js/main.f98e6488.chunk.js"
+  },
+  {
+    "revision": "8c85bae05d866358124a",
+    "url": "/Keeper-App/static/js/runtime-main.fca5ae5c.js"
+  }
+]);
